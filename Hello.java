0336c73a0c88bdfbad8f86b8431974feb5a5e58e
shiv4809 <87180455+shiv4809@users.noreply.github.com>
2021-07-14 19:34:54 +0530

    change

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class hello
 {
 public static void main(String args[])
 {
-System.out.println("hello","new changes","commiting on remote");
+System.out.println("hello","new changes","commiting on remote",shijij);
 }
 }
 
